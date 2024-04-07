0276deb6d51366c4e8ab1a7e1a95fc9179c13c73
John Nooney <1803534@uad.ac.uk>
2024-04-07 17:41:23 +0100

    Refactor whitespace

--- SampleApplication/ViewModels/ViewModelBase.cs
+++ SampleApplication/ViewModels/ViewModelBase.cs
@@ -17,6 +17,7 @@ namespace SampleApplication.ViewModels
             }
             return false;
         }
+
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
