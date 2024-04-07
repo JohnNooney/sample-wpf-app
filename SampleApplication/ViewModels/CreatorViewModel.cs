11a00734d3eb4a27cf9b85b7c91d7b22b3660c25
John Nooney <1803534@uad.ac.uk>
2024-04-07 20:13:47 +0100

    Add CreatorView and ViewModel

---
+++ SampleApplication/ViewModels/CreatorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication.ViewModels
+{
+    internal class CreatorViewModel : BaseViewModel
+    {
+        public CreatorViewModel()
+        {
+            // TODO 
+        }
+    }
+}
